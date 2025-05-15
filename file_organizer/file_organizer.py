2e1c610d1bb72c68bad14456aad113978db7fc00
Jeziel Honorato <jezielh.ss@gmail.com>
2025-05-14 23:48:00 -0300

    Update file_organizer.py

--- file_organizer/file_organizer.py
+++ file_organizer/file_organizer.py
@@ -5,19 +5,18 @@ def organizarArquivos(pasta):
   pasta_organizada = os.path.join(os.getcwd(), pasta)
 
   for file in os.listdir(pasta_organizada):
-    conferir_arquivo = os.path.join(pasta_organizada, file)
-    nome, extensao = os.path.splitext(file)
-    extensao = extensao[1:]
-    if os.path.isfile(conferir_arquivo):
-      print(f'O arquivo {file} ser movido para pasta {extensao}')
-    elif os.path.isdir(conferir_arquivo):
-      print(f'Pasta: {nome}')
-    
-    pastas_geradas = f'{pasta_organizada}/{extensao}'
-    
-    if not os.path.isdir(pastas_geradas):
-      os.mkdir(pastas_geradas)
-    
-    shutil.move(f'{pasta_organizada}/{file}', f'{pastas_geradas}/{file}')
+    caminho_completo = os.path.join(pasta_organizada, file)
+    if os.path.isfile(caminho_completo):
+      nome, extensao = os.path.splitext(file)
+      extensao = extensao[1:] or "sem_extensao"
+      destino = os.path.join(pasta_organizada, extensao)
+
+      if not os.path.isdir(destino):
+        os.mkdir(destino)
+
+      print(f'O arquivo "{file}" será movido para a pasta "{extensao}"')
+      shutil.move(caminho_completo, os.path.join(destino, file))
+    else:
+      print(f'"{file}" é uma pasta, ignorando.')
 
 organizarArquivos('downloads')
