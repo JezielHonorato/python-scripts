ff81657019b530f944e117993a35817dd2aa384c
Jeziel Honorato <jezielh.ss@gmail.com>
2025-05-15 22:54:17 -0300

    raticiacao

--- calculadora/calculadora.py
+++ calculadora/calculadora.py
@@ -15,19 +15,23 @@ def divisao(x, y):
 def potenciacao(x, y):
   return x ** y
 
+def raticiacao(x, y):
+  return x ** (1/y)
+
 
 def menu():
   print("Escolha a operação:")
-  print("1 - Soma")
-  print("2 - Subtração")
-  print("3 - Multiplicação")
-  print("4 - Divisão")
-  print("5 - Potenciação")
+  print("1 - Soma: A + B")
+  print("2 - Subtração: A - B")
+  print("3 - Multiplicação: A * B")
+  print("4 - Divisão: A / B")
+  print("5 - Potenciação: A ^ B")
+  print("6 - Raticiação: A ^ 1/B")
 
 menu()
 
 try:
-  op = int(input("Digite sua opção (1/2/3/4/5): "))
+  op = int(input("Digite sua opção (1/2/3/4/5/6): "))
 except ValueError:
   print("Erro: Opção inválida")
   exit()
@@ -50,5 +54,7 @@ match op:
     print("Resultado:", divisao(a, b))
   case 5:
     print("Resultado:", potenciacao(a, b))
+  case 6:
+    print("Resultado:", raticiacao(a, b))
   case _:
     print("Opção inválida")
