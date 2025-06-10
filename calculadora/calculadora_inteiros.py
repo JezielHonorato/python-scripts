91e5e734a3e30ca4d1074014f94c64fc617bed9f
Jeziel Honorato <jezielh.ss@gmail.com>
2025-06-09 22:56:10 -0300

    subtração

---
+++ calculadora/calculadora_inteiros.py
@@ -0,0 +1,9 @@
+# Todas as operações matemticas são somas.
+
+def soma(x, y):
+    """Calcula a soma de dois números."""
+    return x + y
+
+def subtracao(x, y):
+    """Calcula a subtração de dois números."""
+    return x + (-y)
