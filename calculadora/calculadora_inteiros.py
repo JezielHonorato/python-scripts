550da408ae9d1c724944a138be3a9f04df537c0c
Jeziel Honorato <jezielh.ss@gmail.com>
2025-06-09 22:56:42 -0300

    divisao

--- calculadora/calculadora_inteiros.py
+++ calculadora/calculadora_inteiros.py
@@ -15,3 +15,19 @@ def multiplicacao(x, y):
         total += x
     return total
 
+def divisao(x, y):
+    """
+    Calcula a divisão de dois números Inteiros.
+    """
+    if y == 0:
+        return "Erro: Divisão por zero não é permitida."
+
+    i = 0
+    while y <= x:
+        x -= y
+        i += 1
+        print(f"x: {x}")
+        print(f"y: {y}")
+        print(f"i: {i}")
+        
+    return f"{i} com resto {x}"
