6bfcfeed1a4d2a00545d3f8bce444da7272d901f
Jeziel Honorato <jezielh.ss@gmail.com>
2025-06-09 22:56:25 -0300

    multiplicação

--- calculadora/calculadora_inteiros.py
+++ calculadora/calculadora_inteiros.py
@@ -7,3 +7,11 @@ def soma(x, y):
 def subtracao(x, y):
     """Calcula a subtração de dois números."""
     return x + (-y)
+
+def multiplicacao(x, y):
+    """Calcula o produto de dois números Inteiros."""
+    total = 0
+    for i in range(y):
+        total += x
+    return total
+
